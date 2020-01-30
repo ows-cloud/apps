3bc0895ce6da83458728fe7a51eaf1ba9d612cd5
Henrik Norlin <henrik.norl@gmail.com>
2020-01-30 22:25:27 +0100

    [12.0][NEW] document_js (attach file anywhere)

---
+++ document_js/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'document_js',
+
+    'author': 'AppsToGROW',
+    'auto_install': True,
+    'category': 'Uncategorized',
+    'data': [
+        'document_templates.xml',
+    ],
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'website': 'http://www.appstogrow.org',
+}
