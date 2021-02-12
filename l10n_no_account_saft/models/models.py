0ff3bb5328432a288925c3a6a88a33cdda3413cc
Henrik Norlin <henrik.norl@gmail.com>
2021-02-12 17:13:59 +0100

    [l10n_no_account_saft] logic draft

--- l10n_no_account_saft/models/models.py
+++ l10n_no_account_saft/models/models.py
@@ -50,9 +50,12 @@ class SaftWizard(models.TransientModel):
             raise UserError(_('The period should have this format: yyyy-mm'))
 
         # Create record with xml
-        d = {'month_from': self.month_from, 'month_to': self.month_to, 'saft_xml': self._create_xml()}
+        d = {'month_from': self.month_from, 'month_to': self.month_to}
         record = self.env['l10n_no_account_saft.xml'].create(d)
-
+        audit_file = AuditFile.AuditFile(record)
+        xml_io = StringIO()
+        audit_file.export(xml_io, level=0)      
+        record.saft_xml = xml_io.getvalue()
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'l10n_no_account_saft.xml',
@@ -61,11 +64,11 @@ class SaftWizard(models.TransientModel):
             'view_mode': 'form',
         }
 
-    def _create_xml(self):
-        audit_file = self.AuditFile()
-        xml_io = StringIO()
-        audit_file.export(xml_io, level=0)
-        return xml_io.getvalue()
+
+class AuditFile:
+
+    def __init__(self, saft_record):
+        self.company = saft_record.company_id
 
     def AuditFile(self):
         audit_file = saft.AuditFile()
@@ -73,6 +76,8 @@ class SaftWizard(models.TransientModel):
         audit_file.MasterFiles = self.MasterFiles()
         audit_file.GeneralLedgerEntries = self.GeneralLedgerEntries()
         return audit_file
+
+
 
     def Header(self):
         h = saft.HeaderStructure()
@@ -95,32 +100,244 @@ class SaftWizard(models.TransientModel):
 
     def Company(self):
         c = saft.CompanyStructure()
-        
-        return c
-    def (self):
+        c.RegistrationNumber = self.company.vat[2:]
+        c.Name = self.company.name
+        c.Address = self.Address(self.company)
+        c.Contact = self.Contact(self.company)
+        c.TaxRegistration = self.TaxRegistration(self.company.partner_id)
+        c.BankAccount = self.BankAccount(self.company.partner_id)
+        # c.Address = saft.AddressStructure()
+        # c.Address.StreetName = self.company.street
+        # c.Address.AdditionalAddressDetail = self.company.street2
+        # c.Address.City = self.company.city
+        # c.Address.PostalCode = self.company.zip
+        # c.Address.Region = self.company.state_id.name or ''
+        # c.Address.Country = self.company.country_id.code or ''
+        # c.Address.AddressType = 'PostalAddress'
 
-    def (self):
+        # c.Contact = saft.ContactHeaderStructure()
+        # c.Contact.ContactPerson = saft.ContactInformationStructure()
+        # c.Contact.ContactPerson.FirstName = self.company.user_tech_id.name.partition(' ')[0]
+        # c.Contact.ContactPerson.LastName = self.company.user_tech_id.name.partition(' ')[-1]
+        # # otherwise try this https://www.codespeedy.com/get-the-last-word-from-a-string-in-python/
+        # c.Contact.Telephone = self.company.user_tech_id.phone
+        # c.Contact.Email = self.company.user_tech_id.email
+        # c.Contact.Website = self.company.user_tech_id.website
+        # c.Contact.MobilePhone = self.company.user_tech_id.mobile
+        
+        # TODO should not depend on l10n_no_hr_payroll
+        c.TaxRegistration = saft.TaxInformationStructure()
+        mvanr = self.company.field_value_hr_ids.filtered(lambda r: r.field_code == 'l10n_no_virksomhet').value
+        c.TaxRegistration.TaxRegistrationNumber = mvanr + 'MVA' 
+        c.TaxRegistration.TaxAuthority = "Skatteetaten"
+        c.TaxRegistration.TaxVerificationDate = self.company.write_date
+
+        # TODO BankAccount
+
+        return c
 
     def MasterFiles(self):
         mf = saft.MasterFilesType()
+
+        mf.GeneralLedgerAccounts = saft.GeneralLedgerAccountsType()
+        for account in self.company.env['account.account'].browse():
+            mf.GeneralLedgerAccounts.append(self.Account(account))
+
+        mf.Customers = saft.CustomersType()
+        for customer in self.company.env['res.partner'].browse().filtered(lambda r: r.is_customer == True):
+            mf.Customers.append(self.Customer(customer))
+
+        mf.Suppliers = saft.SuppliersType()
+        for supplier in self.company.env['res.partner'].browse().filtered(lambda r: r.is_supplier == True):
+            mf.Suppliers.append(self.Supplier(supplier))
+
+        mf.TaxTable = saft.TaxTableType()
+        for tax in self.company.env['account.tax'].browse():
+            mf.TaxTable.append(self.TaxTableEntry(tax))
+
+        mf.AnalysisTypeTable = saft.AnalysisTypeTableType()
+        for analytic in self.company.env['account.analytic.account'].browse():
+            mf.AnalysisTypeTable.append(self.AnalysisTypeTableEntry(analytic))
+
+        mf.Owners = saft.OwnersType()
+        for owner in self.company.env[''].browse():
+            mf.Owners.append(self.Owner(owner))
+
         return mf
 
-    def (self):
+    def Account(self, account):
+        a = saft.AccountType()
+        a.AccountID = account.code
+        a.AccountDescription = account.name
+        a.AccountType = "GL"
+        a.AccountCreationDate = account.create_date
+        a.OpeningDebitBalance = 1000.0 # TODO
+        a.ClosingDebitBalance = 1000.0 # TODO
+        return a
 
-    def (self):
+    def Customer(self, customer):
+        return self.Partner(customer, 'customer')
 
-    def (self):
+    def Supplier(self, supplier):
+        return self.Partner(supplier, 'supplier')
 
-    def (self):
+    def Partner(self, partner, partner_type):
+        if partner_type == 'customer':
+            p = saft.CustomerType()
+        elif partner_type == 'supplier':
+            p = saft.SupplierType()
+        
+        p.RegistrationNumber = partner.vat
+        p.Name = partner.name
+        p.Address = self.Address(partner)
+        p.Contact = self.Contact(partner)
+        p.TaxRegistration = partner.vat[2:] # TODO 'MVA'
+        return p
+
+    def Address(self, partner):
+        a = saft.AddressStructure()
+        a.StreetName = partner.street
+        a.AdditionalAddressDetail = partner.street2
+        a.City = partner.city
+        a.PostalCode = partner.zip
+        a.Region = partner.state_id.name or ''
+        a.Country = partner.country_id.code or ''
+        a.AddressType = 'PostalAddress'
+        return a
+
+    def Contact(self, partner):
+        c = saft.ContactHeaderStructure()
+        c.ContactPerson = saft.ContactInformationStructure()
+        c.ContactPerson.FirstName = self.company.user_tech_id.name.partition(' ')[0]
+        c.ContactPerson.LastName = self.company.user_tech_id.name.partition(' ')[-1]
+        # otherwise try this https://www.codespeedy.com/get-the-last-word-from-a-string-in-python/
+        c.Telephone = self.company.user_tech_id.phone
+        c.Email = self.company.user_tech_id.email
+        c.Website = self.company.user_tech_id.website
+        c.MobilePhone = self.company.user_tech_id.mobile
+        return c
+
+    def TaxRegistration(self, partner):
+        t = saft.TaxInformationStructure()
+        # mvanr = self.company.field_value_hr_ids.filtered(lambda r: r.field_code == 'l10n_no_virksomhet').value
+        t.TaxRegistrationNumber = partner.vat 
+        # c.TaxRegistration.TaxAuthority = "Skatteetaten"
+        # c.TaxRegistration.TaxVerificationDate = self.company.write_date
+        return t
+
+    def BankAccount(self, partner):
+        b = saft.BankAccountStructure()
+        bank_account = partner.bank_ids[0]
+        b.BankAccountNumber = bank_account.acc_number
+        b.BIC = bank_account.bank_bic
+        b.CurrencyCode = bank_account.currency_id.name
+        # b.GeneralLedgerAccountID
+        return b
+
+    def PartyInfo(self, partner, partner_type):
+        p = saft.PartyInfoStructure()
+        p.PaymentTerms = saft.PaymentTermsType()
+        # if partner_type == 'customer':
+        #     p.PaymentTerms.Days = partner.property_payment_term_id...
+        # elif partner_type == 'supplier':
+        #     p.PaymentTerms.Days = partner.property_supplier_payment_term_id...
+        # p.PaymentTerms.CashDiscountDays = partner.
+        # p.PaymentTerms.CashDiscountRate = partner.
+        # p.PaymentTerms.FreeBillingMonth = partner.
+        # p.NaceCode = '63.110'
+        p.CurrencyCode = partner.currency_id.name
+        p.Type = 'Company' if partner.is_company else 'Private'
+        p.Status = 'Active' if partner.active else 'Archived'
+        return p
+
+    def TaxTableEntry(self, tax):
+        t = saft.TaxTableEntryType()
+        t.TaxType = 'MVA'
+        t.Description = tax.name
+        t.TaxCodeDetails = saft.TaxCodeDetailsType()
+        t.TaxCodeDetails.TaxPercentage = tax.amount * 100
+        t.TaxCodeDetails.Country = 'NO'
+        # t.TaxCodeDetails.StandardTaxCode # TODO mandatory
+        t.TaxCodeDetails.BaseRate = 100
+        return t
+
+    def AnalysisTypeTableEntry(self, analytic):
+        a = saft.AnalysisTypeTableEntryType()
+        a.AnalysisType = 'A'
+        a.AnalysisTypeDescription = 'Analytic Account'
+        a.AnalysisID = analytic.code
+        a.AnalysisIDDescription = analytic.name
+        return a
+
+    def Owner(self):
+        o = saft.OwnerType()
+        return o
 
     def GeneralLedgerEntries(self):
-        gle = saft.GeneralLedgerEntriesType()
-        return gle
+        e = saft.GeneralLedgerEntriesType()
+        e.NumberOfEntries = 1 # TODO
+        e.TotalDebit = 1000 # TODO
+        e.TotalCredit = 1000 # TODO
+        e.Journal = saft.JournalType()
+        for journal in self.company.env['account.journal'].browse():
+            e.Journal.append(self.Journal(journal))
+        return e
 
-    def (self):
+    def Journal(self, journal):
+        j = saft.JournalType()
+        j.JournalID = journal.code
+        j.Description = journal.name
+        j.Type = journal.code # ?
+        j.Transaction = saft.TransactionType()
+        for move in self.company.env['account.move'].browse().filtered(lambda r: r.journal_id == journal):
+            j.Transaction.append(self.Transaction(move))
+        return j
 
-    def (self):
+    def Transaction(self, move):
+        t = saft.TransactionType()
+        t.TransactionID = move.name
+        t.Period = move.
+        t.PeriodYear = move.
+        t.TransactionID = move.date
+        t.SourceID = move.
+        t.TransactionType = move.
+        t.Description = move.
+        t.BatchID = move.
+        t.SystemEntryDate = move.
+        t.GLPostingDate = move.
+        t.SystemID = move.
+        for line in move.line_ids:
+            t.append(self.Line(line))
+        return t
 
-    def (self):
+    def Line(self, line):
+        l = saft.LineType()
+        l.RecordID = 1 # TODO
+        l.AccountID = line.account_id.code
+        l.Analysis = saft.AnalysisStructure
+        l.Analysis.AnalysisType = 'A'
+        l.Analysis.AnalysisID = line.analytic_account_id.code
+        l.ValueDate = line.move_id.date
+        # l.SourceDocumentID = 
+        l.Description = line.name
+        l.DebitAmount = saft.DebitCreditIndicatorType()
+        l.DebitAmount.Amount = line.debit
+        l.CreditAmount = saft.DebitCreditIndicatorType()
+        l.CreditAmount.Amount = line.credit
+        for tax in line.tax_ids:
+            l.append(self.TaxInformation(tax))
+        # l.ReferenceNumber
+        # l.CID
+        l.SystemEntryTime = datetime.now()
+        # l.ownerID
+        return l
 
-    def (self):
+    def TaxInformation(self, move, tax):
+        t = saft.TaxInformationStructure()
+        t.TaxType = 'MVA'
+        # t.TaxCode
+        t.TaxPercent = tax.amount * 100
+        # t.TaxBase
+        t.TaxAmount = saft.TaxAmount() # doesn't exist!
+        # t.TaxAmount.Amount
+        return t
