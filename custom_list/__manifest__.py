24f6d267bc682dbc2a27ef81f1b9bbb63d3d4c7d
Henrik Norlin <henrik.norl@gmail.com>
2021-08-09 22:10:08 +0200

    [DEV] custom_list

---
+++ custom_list/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2019 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom List',
+    'summary': 'For budget or statistics',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+        'views/views.xml',
+        'views/granheim.xml',
+    ],
+    'depends': [
+        'account',
+        'analytic',
+        'uom',
+    ],
+    'license': 'AGPL-3',
+    'version': '12.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+
+    'description': '''
+
+    ''',
+}
