c59a106f39a660293c89d261bd496512324b18ed
Henrik <henrik.norl@gmail.com>
2021-08-09 23:54:23 +0200

    [FIX] custom_list'

--- custom_list/models/models.py
+++ custom_list/models/models.py
@@ -7,14 +7,12 @@ class CustomList(models.Model):
     company_id = fields.Many2one('res.company', string="Company", index=True, default=lambda self: self.env.user.company_id)
     name = fields.Char()
     type = fields.Selection([('budget', 'Budget'), ('other', 'Other')])
-    uom = fields.Many2one('uom.uom', string="Unit of Measure")
 
 
 class CustomListItem(models.Model):
     _name = 'custom.list.item'
 
     name = fields.Char()
-    uom = fields.Many2one('uom.uom', string="Unit of Measure", readonly=True, related='list_id.uom')
 
     account_id = fields.Many2one('account.account', string="Account")
     analytic_account_id = fields.Many2one('account.analytic.account', string="Anaytic Account")
@@ -26,7 +24,4 @@ class CustomListItem(models.Model):
         return self.env.user
     user_id = fields.Many2one('res.users', string="User", default=compute_default_user)
 
-    #char = fields.Char()
     float = fields.Float()
-    #integer = fields.Integer()
-    #monetary = fields.Monetary()
