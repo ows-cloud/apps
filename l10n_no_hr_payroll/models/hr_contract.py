542d2b027293bb7f802b3e51fafd7d859f4e9cbf
Henrik Norlin <henrik.norl@gmail.com>
2021-08-25 07:23:52 +0200

    [IMP] l10n_no_hr_payroll: hr.contract new computed fields

--- l10n_no_hr_payroll/models/hr_contract.py
+++ l10n_no_hr_payroll/models/hr_contract.py
@@ -6,6 +6,13 @@ class HrContract(models.Model):
 
     leave_ids = fields.One2many('hr.leave', 'contract_id', string='Leaves')
     l10n_no_arbeidsforhold = fields.Char(help="A-melding: ArbeidsforholdId")
+    l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer = fields.Float(compute='compute_l10n_no_fields')
+    l10n_no_stillingsprosent = fields.Float(compute='compute_l10n_no_fields')
+
+    def compute_l10n_no_fields(self):
+        for record in self:
+            record.l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer = float(record.field_value_ids.filtered(lambda x: x.field_code == "l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer").value)
+            record.l10n_no_stillingsprosent = float(record.field_value_ids.filtered(lambda x: x.field_code == "l10n_no_stillingsprosent").value)
 
     def copy(self, default=None):
         default = dict(default or {})
