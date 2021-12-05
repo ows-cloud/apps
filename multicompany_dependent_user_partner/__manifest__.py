4f2e83fd9eae54496ab693a7b6eeb75097043e24
Henrik Norlin <henrik.norl@gmail.com>
2021-12-05 12:35:38 +0100

    [ADD] multicompany_dependent_user_partner

---
+++ multicompany_dependent_user_partner/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'NAME',
+    'summary': 'SUMMARY',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+        # 'views/res.users.xml',
+    ],
+    'depends': [
+        'base',
+    ],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+}
