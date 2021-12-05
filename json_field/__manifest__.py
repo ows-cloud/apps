83d0c22c68d538a5f8515f3868c3bdb0c3db8ea5
Henrik Norlin <henrik.norl@gmail.com>
2021-12-05 12:35:03 +0100

    [ADD] json_field

---
+++ json_field/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'JSON field',
+    'summary': 'Custom field type',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+        'json.xml',
+    ],
+    'depends': [
+        'base',
+        'web',
+    ],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+}
