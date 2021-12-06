5bc521dadbdd1f125e9de3f2a6c4d63563b7808a
Henrik Norlin <henrik.norl@gmail.com>
2021-12-06 21:14:38 +0100

    deleted account_new_fields, mail_new_fields

--- mail_new_fields/models.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class MailMessage(models.Model):
-    _inherit = 'mail.message'
-
-    company_id = fields.Many2one('res.company', string='Company', store=True, index=True, default=lambda self: self.env.company)
