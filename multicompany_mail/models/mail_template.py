7e2c38348ddfc6a2065f6b50935d335af2ba3224
Henrik Norlin <henrik.norl@gmail.com>
2021-12-06 21:49:41 +0100

    multicompany_mail: _sql_constraints fix replace_record_id

--- multicompany_mail/models/mail_template.py
+++ multicompany_mail/models/mail_template.py
@@ -7,4 +7,4 @@ class MailTemplate(models.Model):
     replace_record_id = fields.Many2one('mail.template', string='Replace Record')
     company_id = fields.Many2one('res.company', string='Company', store=True, index=True, default=lambda self: self.env.company)
     
-    _sql_constraints = [('replace_mail_template_uniq', 'unique(replace_record, company_id)', 'Replace Record must be unique per company!')]
+    _sql_constraints = [('replace_mail_template_uniq', 'unique(replace_record_id, company_id)', 'Replace Record must be unique per company!')]
