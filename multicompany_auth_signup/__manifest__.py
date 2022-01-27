85b1089bece820cced659f80abb2995f28cf48e7
Henrik Norlin <henrik.norl@gmail.com>
2022-01-27 21:27:58 +0100

    multicompany_auth_signup: Field access to company managers to create users

---
+++ multicompany_auth_signup/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'multicompany_auth_signup',
+
+    'author': 'AppsToGROW',
+    'auto_install': True,
+    'category': 'Uncategorized',
+    'data': [
+    ],
+    'depends': [
+        'auth_signup',
+        'multicompany_base',
+    ],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+}
