14408951facdf27dbe5e39d3ccdd74054ead0dc5
Henrik Norlin <henrik.norl@gmail.com>
2022-01-29 23:32:25 +0100

    multicompany_base: show parent view

--- multicompany_base/models/ir_ui_view.py
+++ multicompany_base/models/ir_ui_view.py
@@ -6,9 +6,13 @@ class View(models.Model):
 
     @api.model
     def _get_inheriting_views_arch_domain(self, model):
+        company_id_list = [1, self.env.company.id]
+        parent_company = self.env.company.parent_id
+        if parent_company:
+            company_id_list.append(parent_company.id)
         return [
             ['model', '=', model],
             ['mode', '=', 'extension'],
             ['active', '=', True],
-            ['company_id', 'in', (1, self.env.company.id)],
+            ['company_id', 'in', tuple(company_id_list)],
         ]
