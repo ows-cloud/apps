87ef02092c62b729b4af7e564217c9b12aaa2774
Henrik Norlin <henrik.norl@gmail.com>
2022-02-25 10:58:55 +0100

    payroll_new_fields (hr.payslip.line): date_from, views

--- payroll_new_fields/hr_payslip_line.py
+++ payroll_new_fields/hr_payslip_line.py
@@ -4,4 +4,5 @@ from odoo import models, fields, api
 class HrPayslipLine(models.Model):
     _inherit = 'hr.payslip.line'
 
+    date_from = fields.Date("Date From", related='slip_id.date_from', store=True)
     payslip_run_id = fields.Many2one('hr.payslip.run', related='slip_id.payslip_run_id', string="Payslip Batch")
