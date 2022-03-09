60794a98636b9c71c996c6cba3736c5bc5327757
Henrik Norlin <henrik.norl@gmail.com>
2022-03-09 11:39:59 +0100

    account_fix: no journal number in draft state; date = invoice_date

--- account_fix/models/account_move.py
+++ account_fix/models/account_move.py
@@ -10,3 +10,13 @@ class AccountMove(models.Model):
         # From sequence_mixin
         _sequence_fixed_regex = r'^(?P<prefix1>.*?)(?P<seq>\d{0,9})(?P<suffix>\D*?)$'
         return _sequence_fixed_regex
+
+    @api.depends('posted_before', 'state', 'journal_id', 'date')
+    def _compute_name(self):
+        state = self.mapped('state')
+        if not 'draft' in state:
+            super(AccountMove, self)._compute_name()
+
+    @api.onchange('invoice_date')
+    def _onchange_invoice_date(self):
+        self.date = self.invoice_date
