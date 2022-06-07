07d4b356ab1b06e535d2456e4ecd9d616132e0d2
Henrik Norlin <henrik.norl@gmail.com>
2022-06-07 21:47:08 +0200

    [ADD] pms_fix

---
+++ pms_fix/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'PMS Fix',
+    'summary': '',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+
+    ],
+    'depends': [
+        'pms',
+    ],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+
+    'description': '''
+    ''',
+}
