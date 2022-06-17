dfea72affb3479a194ebff3f6554707242d91870
Henrik Norlin <henrik.norl@gmail.com>
2022-06-17 04:18:25 +0200

    [IMP] calendar_event_matrix: partner_count

--- calendar_event_matrix/models/calendar_event.py
+++ calendar_event_matrix/models/calendar_event.py
@@ -5,6 +5,7 @@ class CalendarEvent(models.Model):
     _inherit = 'calendar.event'
 
     categ_id = fields.Many2one('calendar.event.type', string="Main tag", compute='_get_categ_id') #, inverse='_set_categ_id')
+    partner_count = fields.Integer("No of attendees", compute='_get_partner_count')
     start_date_str = fields.Char('Start Date (text)', compute='_get_start_date_str')
 
     def _get_categ_id(self):
@@ -14,6 +15,10 @@ class CalendarEvent(models.Model):
     def _set_categ_id(self):
         for record in self:
             record.categ_ids = [(4, record.categ_id.id)]
+
+    def _get_partner_count(self):
+        for record in self:
+            record.partner_count = len(record.partner_ids)
 
     def _get_start_date_str(self):
         for record in self:
