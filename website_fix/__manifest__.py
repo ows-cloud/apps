0162c0aa34d10fca700ec63f1aa1340686db0af2
Henrik Norlin <henrik.norl@gmail.com>
2022-07-06 17:45:55 +0200

    [ADD] website_fix

---
+++ website_fix/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Fix',
+    'summary': 'Removed .sudo()',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+
+    ],
+    'depends': [
+        'website_sale',
+    ],
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+
+    'description': '''
+    ''',
+}
---
+++ website_fix/models/website.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    @api.model
+    def sale_get_payment_term(self, partner):
+        """ Removed .sudo()"""
+        pt = self.env.ref('account.account_payment_term_immediate', False)
+        if pt:
+            pt = (not pt.company_id.id or self.company_id.id == pt.company_id.id) and pt
+        return (
+            partner.property_payment_term_id or
+            pt or
+            self.env['account.payment.term'].sudo().search([('company_id', '=', self.company_id.id)], limit=1)
+        ).id
