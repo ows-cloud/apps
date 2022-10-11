2e198f6d8592df975b1e3b2fb697c2d1166b0164
Henrik Norlin <henrik.norl@gmail.com>
2022-10-11 11:08:54 +0200

    multicompany_base: git commit patches

--- multicompany_base/git-base.sh
+++ multicompany_base/git-base.sh
@@ -1,10 +1,18 @@
 source git-config.sh
 cd $path_odoo
 
+# The last commit in the ALL branch:
+# - Apply to the BASE branch.
+# - Update the ALL patch and the BASE patch, and commit them.
 git checkout $branch_all
-git diff $branch_main $branch_all > $patch_all
 COMMIT_ID=$(git rev-parse --verify HEAD)
+COMMIT_MESSAGE=$(git log -1 --pretty=format:%B)
 git checkout $branch_base
 git cherry-pick $COMMIT_ID
 git diff $branch_main $branch_base > $patch_base
 git checkout $branch_all
+git diff $branch_main $branch_all > $patch_all
+
+cd $path_patches
+git add ./*
+git commit -m $COMMIT_MESSAGE
