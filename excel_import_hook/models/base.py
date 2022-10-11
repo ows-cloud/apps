ef764d6b91cfd3c6161eb647b03db2ee78a90155
Henrik Norlin <henrik.norl@gmail.com>
2022-10-11 14:15:44 +0200

    [DEV] excel_import_hook: call multiple methods

--- excel_import_hook/models/base.py
+++ excel_import_hook/models/base.py
@@ -1,16 +1,11 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class Base(models.AbstractModel):
     _inherit = "base"
 
-    def excel_post_import_hook(self, one2many_field_name):
+    def excel_post_import_hook(self, one2many_field_name, _excel_post_import_hook_=[]):
         for record in self:
             for line in getattr(record, one2many_field_name):
-                line.excel_post_import_hook_for_record_line()
-            record.excel_post_import_hook_for_record()
-
-    def _excel_post_import_hook_for_record(self):
-        self.ensure_one()
-
-    def _excel_post_import_hook_for_record_line(self):
-        self.ensure_one()
+                for name in _excel_post_import_hook_:
+                    method = "_excel_post_import_hook_{}".format(name)
+                    api.call_kw(self, method, [line.ids], {})
