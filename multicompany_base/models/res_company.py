06df93ecd4db7fa7678d337628479c54e3ab64f8
Henrik Norlin <henrik.norl@gmail.com>
2022-10-12 10:35:50 +0200

    multicompany_base: Create new company FIX add support user

--- multicompany_base/models/res_company.py
+++ multicompany_base/models/res_company.py
@@ -10,16 +10,17 @@ class Company(models.Model):
 
     @api.model
     def create(self, vals):
-        try:
-            self_sudo_bypass_global_rules = self.sudo(bypass_global_rules=True)
-        except:
-            self_sudo_bypass_global_rules = self.sudo()
-        new_company = super(Company, self_sudo_bypass_global_rules).create(vals)
+        def sudo_bypass_global_rules(obj):
+            try:
+                return self.sudo(bypass_global_rules=True)
+            except:
+                return self.sudo()
+        new_company = super(Company, sudo_bypass_global_rules(self)).create(vals)
         new_company.sudo().partner_id.write({'company_id': new_company.id})
 
         # Give access to SUPPORT USER
         support_user = self.env.ref('__multicompany_base__.support_user')
-        support_user.sudo().write({'company_ids': [(4, new_company.id)]})
+        sudo_bypass_global_rules(support_user).write({'company_ids': [(4, new_company.id)]})
 
         # Auto-configure company
         config = self.env['ir.config_parameter'].sudo().get_param('multicompany_base.force_config')
