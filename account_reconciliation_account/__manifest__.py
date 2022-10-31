8afb9f75a32ee652a50ef5b653554b5cc95f6cd4
Henrik Norlin <henrik.norl@gmail.com>
2022-10-31 17:43:36 +0100

    account_reconciliation_account ready to use

--- account_reconciliation_account/__manifest__.py
+++ account_reconciliation_account/__manifest__.py
@@ -1,18 +1,20 @@
-# Copyright 2019 AppsToGROW - Henrik Norlin
+# Copyright 2022 AppsToGROW - Henrik Norlin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Reconciliation Account',
-    'summary': '',
+    "name": "Account Reconciliation Account",
+    "summary": "",
 
-    'author': 'AppsToGROW',
-    'category': 'account',
-    'data': [
+    "author": "AppsToGROW",
+    "category": "account",
+    "data": [
+        "views/account_bank_statement_views.xml",
+        "views/account_reconcile_model_views.xml",
     ],
-    'depends': [
-        'account_reconciliation_widget',
+    "depends": [
+        "account_reconciliation_widget",
     ],
-    'license': 'AGPL-3',
-    'version': '14.0.1.0.0',
-    'website': 'http://www.appstogrow.co',
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "website": "http://www.appstogrow.co",
 }
