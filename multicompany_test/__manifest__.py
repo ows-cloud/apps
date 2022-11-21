44b200ac01bb71131c5e08307a9a0287bc8aeddf
Henrik Norlin <henrik.norl@gmail.com>
2022-11-21 06:20:54 +0100

    [ADD] multicompany_test

---
+++ multicompany_test/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "multicompany_test",
+    "summary": "Company & User",
+
+    "author": "AppsToGROW",
+    "category": "Administration",
+    "data": [
+        "data/data.xml",
+    ],
+    "depends": ["multicompany_base"],
+    "license": "AGPL-3",
+    "post_init_hook": "post_init_hook",
+    "version": "14.0.1.0.0",
+    "website": "http://www.appstogrow.co",
+}
