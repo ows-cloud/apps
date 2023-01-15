3cb36ccc8209ccc583ac388610fab22815c0f00c
Henrik Norlin <henrik.norl@gmail.com>
2023-01-15 11:10:33 +0100

    mail_alias_defaults: pre_init_hook: try, pass if exception

--- mail_alias_defaults/hooks.py
+++ mail_alias_defaults/hooks.py
@@ -2,15 +2,19 @@ from openupgradelib import openupgrade
 
 
 def pre_init_hook(cr):
-    openupgrade.rename_columns(
-        cr,
-        {
-            "mail_alias":
-            [
-                (
-                    "alias_defaults",
-                    "alias_defaults_input",
-                )
-            ]
-        }
-    )
+    try:
+        openupgrade.rename_columns(
+            cr,
+            {
+                "mail_alias":
+                [
+                    (
+                        "alias_defaults",
+                        "alias_defaults_input",
+                    )
+                ]
+            }
+        )
+    except Exception:
+        # alias_defaults_input may already exist.
+        pass
