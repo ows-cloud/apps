22814b220f4de2aabded1e3338e2a4b48b39a050
Henrik Norlin <henrik.norl@gmail.com>
2023-01-18 23:30:01 +0100

    [DEV] l10n_no_payroll: post_init_hook_import_job_codes

--- l10n_no_payroll/hooks.py
+++ l10n_no_payroll/hooks.py
@@ -3,5 +3,4 @@ from odoo import SUPERUSER_ID, api
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env["l10n.no.job.code"].import_job_codes()
-
+    env["l10n.no.job.code"].post_init_hook_import_job_codes()
--- l10n_no_payroll/models/l10n_no_job_code.py
+++ l10n_no_payroll/models/l10n_no_job_code.py
@@ -1,3 +1,6 @@
+import inspect
+import os
+
 from odoo import fields, models
 
 
@@ -8,32 +11,40 @@ class JobCode(models.Model):
     name = fields.Char()
     code = fields.Char()
 
-    def import_job_codes(self):
-
-        import_record = self.env['base_import.import'].create({
-            # "file": file.read(),
-            "file": b"name,code\nHenrik Norlin,+4791120745\n",
-            "file_name": "l10n.no.job.code.csv",
-            "file_type": "text/csv",
-        })
-        import_record.do(
-            columns="name,code",
-            fields="name,code",
-            options={
-                'headers': True,
-                'advanced': False,
-                'keep_matches': False,
-                'name_create_enabled_fields': {},
-                'skip': 0,
-                'limit': 2000,
-                'encoding': 'ascii',
-                'separator': ',',
-                'quoting': '"',
-                'sheet': '',
-                'date_format': '',
-                'datetime_format': '',
-                'float_thousand_separator': ',',
-                'float_decimal_separator': '.',
-                'fields': [],
-            },
-        )
+    def post_init_hook_import_job_codes(self):
+        directory_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        file_path =  os.path.join(directory_path, '../data/l10n.no.job.code.csv')
+        with open(file_path, "r") as file:
+            import_record = self.env['base_import.import'].create(
+                {
+                    "file": file.read(),
+                    "file_name": "l10n.no.job.code.csv",
+                    "file_type": "text/csv",
+                    "res_model": "l10n.no.job.code",
+                }
+            )
+            import_record.do(
+                columns=["code", "parentCode", "level", "name", "shortName", "notes", "validFrom", "validTo"],
+                fields=["code", False, False, "name", False, False, False, False],
+                # options changed:
+                # - separator ;
+                # - encoding utf-8
+                # - limit 10000
+                options={
+                    'headers': True,
+                    'advanced': False,
+                    'keep_matches': False,
+                    'name_create_enabled_fields': {},
+                    'skip': 0,
+                    'limit': 10000,
+                    'encoding': 'utf-8',
+                    'separator': ';',
+                    'quoting': '"',
+                    'sheet': '',
+                    'date_format': '',
+                    'datetime_format': '',
+                    'float_thousand_separator': ',',
+                    'float_decimal_separator': '.',
+                    'fields': [],
+                },
+            )
