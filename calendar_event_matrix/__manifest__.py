315c7872845ce71a7a283ae66b4c21a18ee87f1b
Henrik Norlin <henrik.norl@gmail.com>
2023-06-09 14:35:52 +0200

    calendar_event_matrix: calendar.attendee views

--- calendar_event_matrix/__manifest__.py
+++ calendar_event_matrix/__manifest__.py
@@ -15,6 +15,7 @@
         "security/security.xml",
         "security/ir.model.access.csv",
         "views/calendar.event.matrix.xml",
+        "views/calendar_attendee_views.xml",
         "report/report_templates.xml",
         "report/report_views.xml",
     ],
