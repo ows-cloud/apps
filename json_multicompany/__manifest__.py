e5f871cfef2adb0627e6d4d5bfde3e705b30bdb9
Henrik Norlin <henrik.norl@gmail.com>
2023-06-13 14:09:44 +0200

    [ADD] json_field, json_multicompany

---
+++ json_multicompany/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "json_multicompany",
+    "summary": "",
+
+    "author": "AppsToGROW",
+    "category": "Uncategorized",
+    "data": [
+        "views/ir_model_fields_views.xml",
+    ],
+    "depends": [
+        "company_field",
+        "json_field",
+    ],
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "website": "http://www.appstogrow.co",
+}
