6938d5bc072d984d0d797df941bff3c1bd6688ab
Henrik Norlin <henrik.norl@gmail.com>
2023-06-13 14:12:43 +0200

    [ADD] company_field

---
+++ company_field/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Company field',
+    'summary': 'company_id field on all models',
+
+    'author': 'AppsToGROW',
+    'category': 'Administration',
+    'data': [
+    ],
+    'depends': [
+        'base',
+    ],
+    'development_status': 'Alpha',
+    'license': 'AGPL-3',
+    'version': '14.0.1.0.0',
+    'website': 'http://www.appstogrow.co',
+}
