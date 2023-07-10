fb96d7e4193d94db4a45f463f2a353207ac6c41f
Henrik Norlin <henrik.norl@gmail.com>
2023-07-10 21:41:17 +0200

    payroll_payslip_line: pre_init_hook

--- payroll_payslip_line/__manifest__.py
+++ payroll_payslip_line/__manifest__.py
@@ -13,6 +13,7 @@
     "depends": ["payroll"],
     "license": "AGPL-3",
     "maintainers": ["appstogrow"],
+    "pre_init_hook": "pre_init_hook",
     "version": "14.0.1.0.0",
     "website": "https://github.com/appstogrow/apps",
 }
