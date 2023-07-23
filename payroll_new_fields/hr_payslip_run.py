1dcfc82b486c50cf1939c8df86e17a27ba0123ba
Henrik Norlin <henrik.norl@gmail.com>
2023-07-23 07:14:49 +0200

    June-July 2023: payroll_new_fields > payroll_payslip_line

--- payroll_new_fields/hr_payslip_run.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-
-class HrPayslipLine(models.Model):
-    _inherit = "hr.payslip.run"
-    _order = "date_start desc"
--- payroll_payslip_line/__manifest__.py
+++ payroll_payslip_line/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "payroll_new_fields",
-    "summary": "payslip_line.run_id",
+    "name": "Payslip Line with more fields",
+    "summary": "",
     "author": "AppsToGROW, " "Odoo Community Association (OCA)",
     "auto_install": False,
     "category": "Uncategorized",
     "data": [
-        "hr.payslip.line.xml",
+        "views/hr_payslip_line_views.xml",
     ],
     "depends": ["payroll"],
     "license": "AGPL-3",
