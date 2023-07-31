17fc3255edfc5893e69504e213ff26e557913952
Henrik Norlin <henrik.norl@gmail.com>
2023-07-31 10:06:35 +0200

    l10n_no_payroll: trying to fix l10n_no_man_months, how to force compute?

--- l10n_no_payroll/models/hr_payslip_line.py
+++ l10n_no_payroll/models/hr_payslip_line.py
@@ -13,7 +13,7 @@ class HrPayslipLine(models.Model):
     _inherit = "hr.payslip.line"
 
     l10n_no_man_months = fields.Float(
-        "Man Months", compute="_compute_man_months", store=True
+        "Man Months", compute="_compute_man_months", store=True, default=lambda self: self._compute_man_months(),
     )
     l10n_no_total_ytd = fields.Float(
         "Total, year to date", compute="_compute_l10n_no_total_ytd"
@@ -22,33 +22,21 @@ class HrPayslipLine(models.Model):
     def _compute_man_months(self):
         for line in self:
             rule = line.salary_rule_id
-            code_field = rule.field_value_ids.filtered(
-                lambda x: x.field_code == "l10n_no_Loennsbeskrivelse"
-            )
-            if len(code_field) == 1:
-                code = code_field.selection_value_id.code
-                if code == "fastloenn":
-                    line.l10n_no_man_months = line.quantity * line.rate / 100.0
-                elif code == "timeloenn":
-                    try:
-                        hours_per_week = line.contract_id.field_value_ids.filtered(
-                            lambda x: x.field_code
-                            == "l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer"
-                        ).value
-                    except:
-                        raise UserError(
-                            _("Contract {} (id {}) is missing field l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer.").format(
-                                line.contract_id.name, line.contract_id.id
-                            )
-                        )
-                    weeks_per_year = 52.0
-                    months_per_year = 12.0
-                    hours_per_month = (
-                        float(hours_per_week) * weeks_per_year / months_per_year
-                    )
-                    line.l10n_no_man_months = (
-                        line.quantity * line.rate / 100.0 / hours_per_month
-                    )
+            code = line.l10n_no_Loennsbeskrivelse
+            if code == "fastloenn":
+                line.l10n_no_man_months = line.quantity * line.rate / 100.0
+            elif code == "timeloenn":
+                hours_per_week = line.l10n_no_antallTimerPerUkeSomEnFullStillingTilsvarer
+                weeks_per_year = 52.0
+                months_per_year = 12.0
+                hours_per_month = (
+                    float(hours_per_week) * weeks_per_year / months_per_year
+                )
+                line.l10n_no_man_months = (
+                    line.quantity * line.rate / 100.0 / hours_per_month
+                )
+            if len(self) == 1:
+                return line.l10n_no_man_months
 
     def _compute_l10n_no_total_ytd(self):
         for line in self:
