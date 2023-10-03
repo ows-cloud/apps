e3153f825abc1fd10bf4d4813e77ccba5e1d021a
Henrik Norlin <henrik.norl@gmail.com>
2023-10-03 21:14:02 +0200

    [DEL] json_field, json_multicompany

--- json_field/models/base.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-from odoo.addons.base_sparse_field.models.fields import Serialized
-
-
-class Base(models.AbstractModel):
-    _inherit = "base"
-
-    json = Serialized()
--- json_field/readme/DESCRIPTION.rst
+++
@@ -1,3 +0,0 @@
-This module depends on `base_sparse_field` and creates `json` field on all models.
-
-One use case is allowing 
