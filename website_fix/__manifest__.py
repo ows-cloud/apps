8d820cb35b2f826559ba2c748a974b882b41bc69
Henrik Norlin <henrik.norl@gmail.com>
2023-10-04 14:23:17 +0200

    [15.0] website_fix, [DEL] partner_fix, pms_fix

--- website_fix/__manifest__.py
+++ website_fix/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2019 AppsToGROW - Henrik Norlin
+# Copyright 2019 Ows - Henrik Norlin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Website Fix",
     "summary": "Removed .sudo()",
-    "author": "AppsToGROW, " "Odoo Community Association (OCA)",
+    "author": "Ows, Odoo Community Association (OCA)",
     "category": "Administration",
     "data": [],
     "depends": [
         "website_sale",
     ],
     "license": "AGPL-3",
-    "version": "14.0.1.0.0",
-    "website": "https://github.com/appstogrow/apps",
+    "version": "15.0.1.0.0",
+    "website": "https://github.com/OCA",
 }
