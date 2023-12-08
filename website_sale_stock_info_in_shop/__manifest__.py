6faa96a5b08b044e970eb94c05bcb1c19511702a
Henrik Norlin <henrik.norl@gmail.com>
2023-12-08 14:11:36 +0100

    [ADD] website_sale_stock_info_in_shop

---
+++ website_sale_stock_info_in_shop/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "website_sale_show_sales_and_stock",
+    "summary": "",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "uncategorized",
+    "data": [
+        "views/templates.xml",
+    ],
+    "depends": [
+        "website_sale_stock",
+    ],
+    "license": "AGPL-3",
+    "maintainers": ["ows-cloud"],
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/OCA",
+}
