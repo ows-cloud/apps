d8048e21489df4f5139f0d6002ec159d49d842a4
Henrik Norlin <henrik.norl@gmail.com>
2023-12-08 15:05:13 +0100

    [FIX] website_sale_stock_info_in_shop: fields available for public user

---
+++ website_sale_stock_info_in_shop/models/product_product.py
@@ -0,0 +1,43 @@
+from odoo import _, api, fields, models, SUPERUSER_ID
+
+from datetime import timedelta, time
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_round
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _compute_quantities_dict(self, lot_id, owner_id, package_id, from_date=False, to_date=False):
+        # Public user should see product.qty_available
+        return super(ProductProduct, self.sudo())._compute_quantities_dict(lot_id, owner_id, package_id, from_date, to_date)
+
+
+    sales_count = fields.Float(compute='_compute_sales_count', string='Sold')
+
+    def _compute_sales_count(self):
+        # Public user should see sales_count
+        self = self.sudo()
+
+        r = {}
+        self.sales_count = 0
+        # if not self.user_has_groups('sales_team.group_sale_salesman'):
+        #     return r
+        date_from = fields.Datetime.to_string(fields.datetime.combine(fields.datetime.now() - timedelta(days=365),
+                                                                      time.min))
+
+        done_states = self.env['sale.report']._get_done_states()
+
+        domain = [
+            ('state', 'in', done_states),
+            ('product_id', 'in', self.ids),
+            ('date', '>=', date_from),
+        ]
+        for group in self.env['sale.report'].read_group(domain, ['product_id', 'product_uom_qty'], ['product_id']):
+            r[group['product_id'][0]] = group['product_uom_qty']
+        for product in self:
+            if not product.id:
+                product.sales_count = 0.0
+                continue
+            product.sales_count = float_round(r.get(product.id, 0), precision_rounding=product.uom_id.rounding)
+        return r
