9e40d7017a84c72408100faa3806705b0bd0a043
Henrik Norlin <henrik.norl@gmail.com>
2023-12-12 15:26:53 +0100

    [IMP] fleet_vehicle_odometer: default driver

--- fleet_vehicle_odometer/models/fleet_vehicle.py
+++ fleet_vehicle_odometer/models/fleet_vehicle.py
@@ -12,5 +12,8 @@ class FleetVehicle(models.Model):
         action["views"] = [
             [self.env.ref("fleet_vehicle_odometer.fleet_vehicle_odometer_view_form_create").id, "form"]
         ]
-        action["context"] = {"default_vehicle_id": self.id}
+        action["context"] = {
+            "default_vehicle_id": self.id,
+            "default_driver_id": self.env.user.partner_id.id
+        }
         return action
