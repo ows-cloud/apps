1f7ff5e9f4ca432ef49db3f7bfb8805e9dce4f3b
Henrik Norlin <henrik.norl@gmail.com>
2023-12-19 16:28:44 +0100

    [IMP] hr_contract_leave: field leave.contract_id with domain

--- hr_contract_leave/models/hr_leave.py
+++ hr_contract_leave/models/hr_leave.py
@@ -4,5 +4,9 @@ from odoo import fields, models
 class Leave(models.Model):
     _inherit = "hr.leave"
 
-    contract_id = fields.Many2one("hr.contract", string="Contract")
+    contract_id = fields.Many2one(
+        "hr.contract",
+        string="Contract",
+        domain="[('employee_id', '=', employee_id)]"
+    )
     percent = fields.Float(help="40.0 means 40 percent leave, 60 percent work")
