b7738fe059b361d970f225bb05496d5d2c75249d
Henrik Norlin <henrik.norl@gmail.com>
2024-02-20 21:44:06 +0100

    [FIX] __manifest__.py (double)

--- account_pivot/__manifest__.py
+++ account_pivot/__manifest__.py
@@ -20,25 +20,3 @@
     "version": "15.0.1.0.0",
     "website": "https://github.com/OCA",
 }
-# Copyright 2019-2023 Ows - Henrik Norlin
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-{
-    "name": "Account Pivot",
-    "summary": "Similar to account_move_line_report",
-    "author": "Ows, " "Odoo Community Association (OCA)",
-    "category": "account",
-    "data": [
-        "security/ir.model.access.csv",
-        "security/ir.rule.csv",
-        "views/account_account_views.xml",
-        "views/account_pivot_views.xml",
-    ],
-    "depends": [
-        "account",
-    ],
-    "license": "AGPL-3",
-    "maintainers": ["ows-cloud"],
-    "version": "15.0.1.0.0",
-    "website": "https://github.com/OCA",
-}
